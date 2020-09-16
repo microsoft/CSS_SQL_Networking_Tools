1b680470d2da021f933010acbc7c78d71b9d941f
Jon Burchel <jburchel@microsoft.com>
2020-09-16 13:15:28 -0500

    Revert "Completed reporting for read-only intent redirection"
    This reverts commit 99d33a0e828bd579995ab19131193a91f3d79c33.

--- SQL_Network_Analyzer/SQLNA/TDSParser.cs
+++ SQL_Network_Analyzer/SQLNA/TDSParser.cs
@@ -413,17 +413,6 @@ namespace SQLNA
                                         int ErrorLen = (int)fd.payload[17];
                                         c.ErrorMsg = utility.ReadUnicodeString(fd.payload, 19, ErrorLen);
                                     }
-                                    // process read-only intent server name tokens
-                                    else if (fd.payload[8] == (byte)TDSTokenType.ENVCHANGE)
-                                    {
-                                        if (fd.payload[11] == 21) // 21 is LOGINREDIRECT
-                                        {
-                                            c.RedirectPort = utility.ReadUInt16(fd.payload, 15);
-                                            int ServerLen = fd.payload[17];
-                                            c.RedirectServer = utility.ReadUnicodeString(fd.payload, 19, ServerLen);
-                                            c.hasReadOnlyIntentConnection = true;
-                                        }
-                                    }
                                     //pre-login info from Server. 
                                     // if (tokenOffset(fd.payload, (byte)TDSTokenType.PRELOGINRESPONSE) > 7)  // response header is offset 0..7 - need to fix this routine
                                     else if (fd.payload[8] == (byte)TDSTokenType.PRELOGINRESPONSE) // only 1 token in the payload
@@ -490,12 +479,15 @@ namespace SQLNA
                                                 {
                                                     nameLength = fd.payload[offset + 4];
                                                     c.databaseName = utility.ReadUnicodeString(fd.payload, offset + 5, nameLength);
-                                                    break;
                                                 }
-                                                else
+                                                else if (EnvChangeType == 0x14)  // server redirection
                                                 {
-                                                    offset = tokenOffset(fd.payload, (byte)TDSTokenType.ENVCHANGE, offset + tokenLength + 3);
+                                                        c.RedirectPort = utility.ReadUInt16(fd.payload, offset + 7);
+                                                        int ServerLen = fd.payload[offset + 9];
+                                                        c.RedirectServer = utility.ReadUnicodeString(fd.payload, offset + 11, ServerLen);
+                                                        c.hasReadOnlyIntentConnection = true;
                                                 }
+                                                offset = tokenOffset(fd.payload, (byte)TDSTokenType.ENVCHANGE, offset + tokenLength + 3);
                                             }
                                         }
                                         catch (IndexOutOfRangeException ex)
@@ -511,7 +503,6 @@ namespace SQLNA
                                             if (fd.isFromClient == false && c.hasApplicationData == false && c.hasPostLoginResponse == false) switchClientServer++;
                                         }
                                     }
-
 
                                     //accumulate the payload. 
                                     payLoadLength += fd.payload.Length;
